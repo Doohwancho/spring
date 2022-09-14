e594a0a39b649f671b6cf937da4d7ad3dc959c48
Doohwancho <doohwancho526@gmail.com>
2022-09-14 21:50:35 +0900

    B-e-1: stage2 init

---
+++ spring-fundamental/thread/src/main/java/stage2/HelloWorldService.java
@@ -0,0 +1,12 @@
+package stage2;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class HelloWorldService {
+
+    public String helloWorld() {
+        return "Hello World";
+    }
+}
+
