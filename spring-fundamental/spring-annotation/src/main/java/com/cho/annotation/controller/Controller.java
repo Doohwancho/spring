5c588c45f26619b6bd8f6bed3ddd04591d419a33
Doohwancho <doohwancho526@gmail.com>
2022-09-29 20:58:34 +0900

    B-l-5: @CrossOrigin

--- spring-fundamental/spring-annotation/src/main/java/com/cho/annotation/controller/Controller.java
+++ spring-fundamental/spring-annotation/src/main/java/com/cho/annotation/controller/Controller.java
@@ -2,6 +2,7 @@ package com.cho.annotation.controller;
 
 import com.cho.annotation.domain.User;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,19 +10,19 @@ import org.springframework.web.bind.annotation.RequestBody;
 public class Controller {
 
 
-    //case1
+    //case1) x-www-form-urlencoded
     @GetMapping("/user")
     public void x_www_form_urlencoded(String username, String password){
 
     }
 
-    //case2
+    //case2) plain/text (raw)
     @GetMapping("/user")
     public void text_plain_raw_type(@RequestBody String data){
         //parse data
     }
 
-    //case3
+    //case3) application/json
     @GetMapping("/user")
     public void json_application(@RequestBody User user){
 
@@ -32,5 +33,11 @@ public class Controller {
     @GetMapping("/user/{id}")
     public void findById(@PathVariable int id) { //@PathVariable은 저 url에서 {}로만 감싸면, {id}를 int로 파싱해서 넣어줌.
         System.out.println("findById() : id : "+id);
+    }
+
+    @CrossOrigin
+    @GetMapping("/user")
+    public void corsHandler(){
+
     }
 }
