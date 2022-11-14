75ed808dddf7abdb7b7643c1518a457f2f78504f
Doohwancho <doohwancho526@gmail.com>
2022-11-14 19:38:51 +0900

    K-b-4: wikimedia-consumer-jpa

---
+++ message-queue/kafka/springboot-kafka/wikimedia-kafka-consumer-jpa/src/main/java/com/cho/springboot/repository/WikimediaDataRepository.java
@@ -0,0 +1,7 @@
+package com.cho.springboot.repository;
+
+import com.cho.springboot.entity.WikimediaData;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WikimediaDataRepository extends JpaRepository<WikimediaData, Long> {
+}
--- message-queue/kafka/springboot-kafka/wikimedia-kafka-producer/src/main/java/com/cho/springboot/ProducerApplication.java
+++ message-queue/kafka/springboot-kafka/wikimedia-kafka-producer/src/main/java/com/cho/springboot/ProducerApplication.java
@@ -7,10 +7,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application implements CommandLineRunner {
+public class ProducerApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class);
+        SpringApplication.run(ProducerApplication.class);
     }
 
     @Autowired
