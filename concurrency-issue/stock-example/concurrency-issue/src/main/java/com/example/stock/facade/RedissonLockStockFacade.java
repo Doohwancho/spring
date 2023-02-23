dbfe0bd0e6ad111497fea3a9cd35524a476a6095
Doohwancho <doohwancho526@gmail.com>
2023-02-23 19:37:12 +0900

    L-a-6: redis - redisson

---
+++ concurrency-issue/stock-example/concurrency-issue/src/main/java/com/example/stock/facade/RedissonLockStockFacade.java
@@ -0,0 +1,40 @@
+package com.example.stock.facade;
+
+import com.example.stock.service.java.StockService;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.TimeUnit;
+
+@Component
+public class RedissonLockStockFacade {
+
+    private RedissonClient redissonClient;
+
+    private StockService stockService;
+
+    public RedissonLockStockFacade(RedissonClient redissonClient, StockService stockService) {
+        this.redissonClient = redissonClient;
+        this.stockService = stockService;
+    }
+
+    public void decrease(Long key, Long quantity) {
+        RLock lock = redissonClient.getLock(key.toString());
+
+        try {
+            boolean available = lock.tryLock(10, 1, TimeUnit.SECONDS); //lock 획득 최대 대기시간 10초, 안에 들어갔다가 timeout되서 나오는 시간 1초
+
+            if (!available) {
+                System.out.println("lock 획득 실패");
+                return;
+            }
+
+            stockService.decrease(key, quantity);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            lock.unlock();
+        }
+    }
+}
---
+++ concurrency-issue/stock-example/concurrency-issue/src/test/java/com/example/stock/redis/RedissonLockTest.java
@@ -0,0 +1,71 @@
+package com.example.stock.redis;
+
+import com.example.stock.domain.Stock;
+import com.example.stock.facade.LettuceLockStockFacade;
+import com.example.stock.facade.RedissonLockStockFacade;
+import com.example.stock.repository.StockRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+public class RedissonLockTest {
+    //case7) redis: redisson (pub sub)
+    @Autowired
+    private RedissonLockStockFacade stockService;
+
+    @Autowired
+    private StockRepository stockRepository;
+
+
+    @BeforeEach
+    public void insert() {
+        Stock stock = new Stock(1L, 100L);
+
+        stockRepository.saveAndFlush(stock);
+    }
+
+    @AfterEach
+    public void delete() {
+        stockRepository.deleteAll();
+    }
+    /**
+     * Q. what is CountDownLatch?
+     *
+     * A. A synchronization aid that allows one or more threads to wait until a set of operations being performed in other threads completes.
+     */
+
+    @Test
+    @DisplayName("2. 요청이 동시에 100개씩 들어오는 상황")
+    public void 동시에_100명이_주문() throws InterruptedException {
+        int threadCount = 100;
+        ExecutorService executorService = Executors.newFixedThreadPool(32); //ExecuteService는 비동기 요청을 단순화 하는 자바 API
+        CountDownLatch latch = new CountDownLatch(threadCount); //CountDownLatch는 다른 쓰레드의 작업이 완료될 때 까지 기다리게 만드는 클래스
+
+        for (int i = 0; i < threadCount; i++) {
+            executorService.submit(() -> {
+                try {
+                    stockService.decrease(1L, 1L);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+
+        latch.await();
+
+        Stock stock = stockRepository.findById(1L).orElseThrow();
+
+        // 100 - (100 * 1) = 0
+        assertEquals(0, stock.getQuantity()); //expected 0, actual: 88
+    }
+}
