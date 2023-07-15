27fd8980a2beead114188982d2985cdbe031a012
Doohwancho <doohwancho526@gmail.com>
2023-07-16 04:02:31 +0900

    H-d: lotto 당첨 메서드

--- tdd/jwp-lotto/lotto/src/test/java/com/cho/lotto/Lotto.java
+++ tdd/jwp-lotto/lotto/src/test/java/com/cho/lotto/Lotto.java
@@ -1,10 +1,12 @@
 package com.cho.lotto;
 
-import java.math.BigInteger;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Random;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -17,6 +19,10 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 public class Lotto {
 
+  public static final int FOURTH_PRIZE = 5000;
+  public static final int THIRD_PRIZE = 50000;
+  public static final int SECOND_PRIZE = 1500000;
+  public static final int FIRST_PRIZE = 2000000000;
   public static int MIN_BOUND_OF_LOTTO = 1;
   public static int MAX_BOUND_OF_LOTTO = 45;
   public static int LOTTO_SIZE = 6;
@@ -43,6 +49,62 @@ public class Lotto {
     }
     return lists;
   }
+
+  int verifyLottoResult(List<Integer> 당첨번호, List<List<Integer>> 긁은복권모음) throws Exception {
+    if(당첨번호 == null){
+      throw new Exception("당첨번호가 입력되지 않았습니다.");
+    }
+    if(당첨번호.size() != 6){
+      throw new Exception("당첨번호 갯수는 6개여야 합니다.");
+    }
+    if(긁은복권모음 == null){
+      throw new Exception("긁은복권모음이 입력되지 않았습니다.");
+    }
+    if(긁은복권모음.size() == 0){
+      throw new Exception("긁은 복권이 없습니다.");
+    }
+
+    int 당첨금액 = 0;
+
+    Map<Integer, Integer> 당첨통계 = new HashMap<>();
+    당첨통계.put(FOURTH_PRIZE, 0);
+    당첨통계.put(THIRD_PRIZE, 0);
+    당첨통계.put(SECOND_PRIZE, 0);
+    당첨통계.put(FIRST_PRIZE, 0);
+
+    for(List<Integer> 긁은복권 : 긁은복권모음){
+      //step1) 긁은복권에서 당첨번호가 몇개 걸리는지 stream으로 확인
+      int 당첨갯수 = (int)긁은복권.stream().filter(당첨번호::contains).count();
+
+      //step2) 3,4,5,6 인 경우, switch로 당첨통계에 넣기
+      switch(당첨갯수){
+        case 3:
+          당첨통계.put(5000, 1);
+          break;
+        case 4:
+          당첨통계.put(50000, 1);
+          break;
+        case 5:
+          당첨통계.put(1500000, 1);
+          break;
+        case 6:
+          당첨통계.put(2000000000, 1);
+          break;
+      }
+    }
+    //step3) map 돌면서 당첨통계 print
+    System.out.println("당첨 통계");
+    System.out.println("-----------");
+    당첨통계.entrySet().stream().sorted((a, b) -> a.getKey() - b.getKey()).forEach(x -> System.out.println("?개 일치 ("+x.getKey()+")- "+x.getValue()+"개")); //TODO - ?개를 어떻겍 엘레강스하게 표현하지?
+    System.out.println();
+
+    //step4) map 돌면서 당첨금액 합산
+    당첨금액 = 당첨통계.entrySet().stream().mapToInt(e -> e.getKey() * e.getValue()).sum();
+
+    return 당첨금액;
+  }
+
+
 
   @ValueSource(ints = {-1000, -1000000, Integer.MIN_VALUE})
   @ParameterizedTest
@@ -120,5 +182,127 @@ public class Lotto {
 
       Assertions.assertThat(uniqueNumbers.size()).isEqualTo(lotto.size());
     }
+  }
+
+  @Test
+  void 로또_1등_당첨금액_체크() throws Exception {
+    //given
+    List<List<Integer>> 긁은복권모음 = new ArrayList<>();
+    List<Integer> 당첨_6개 = Arrays.asList(1,2,3,4,5,6);
+
+    긁은복권모음.add(당첨_6개);
+
+    List<Integer> 당첨번호 = Arrays.asList(1,2,3,4,5,6);
+
+    //when
+    int result = verifyLottoResult(당첨번호, 긁은복권모음);
+
+    //then
+    Assertions.assertThat(result).isEqualTo(FIRST_PRIZE);
+  }
+
+  @Test
+  void 로또_2등_당첨금액_체크() throws Exception {
+      //given
+      List<List<Integer>> 긁은복권모음 = new ArrayList<>();
+      List<Integer> 당첨_5개 = Arrays.asList(1,2,3,4,5,100);
+
+      긁은복권모음.add(당첨_5개);
+
+      List<Integer> 당첨번호 = Arrays.asList(1,2,3,4,5,6);
+
+      //when
+      int result = verifyLottoResult(당첨번호, 긁은복권모음);
+
+      //then
+      Assertions.assertThat(result).isEqualTo(SECOND_PRIZE);
+  }
+
+
+  @Test
+  void 로또_3등_당첨금액_체크() throws Exception {
+    //given
+    List<List<Integer>> 긁은복권모음 = new ArrayList<>();
+    List<Integer> 당첨_4개 = Arrays.asList(1,2,3,4,100,100);
+
+    긁은복권모음.add(당첨_4개);
+
+    List<Integer> 당첨번호 = Arrays.asList(1,2,3,4,5,6);
+
+    //when
+    int result = verifyLottoResult(당첨번호, 긁은복권모음);
+
+    //then
+    Assertions.assertThat(result).isEqualTo(THIRD_PRIZE);
+  }
+
+
+
+  @Test
+  void 로또_4등_당첨금액_체크() throws Exception {
+    //given
+    List<List<Integer>> 긁은복권모음 = new ArrayList<>();
+    List<Integer> 당첨_3개 = Arrays.asList(1,2,3,100,100,100);
+
+    긁은복권모음.add(당첨_3개);
+
+    List<Integer> 당첨번호 = Arrays.asList(1,2,3,4,5,6);
+
+    //when
+    int result = verifyLottoResult(당첨번호, 긁은복권모음);
+
+    //then
+    Assertions.assertThat(result).isEqualTo(FOURTH_PRIZE);
+  }
+
+  @Test
+  void 당첨되지_않은_경우_금액_체크() throws Exception {
+    //given
+    List<List<Integer>> 긁은복권모음 = new ArrayList<>();
+    List<Integer> 꽝 = Arrays.asList(1,2,100,100,100,100);
+
+    긁은복권모음.add(꽝);
+
+    List<Integer> 당첨번호 = Arrays.asList(1,2,3,4,5,6);
+
+    //when
+    int result = verifyLottoResult(당첨번호, 긁은복권모음);
+
+    //then
+    Assertions.assertThat(result).isEqualTo(0);
+  }
+
+  @Test
+  void 당첨번호가_null이면_throw_Exception() {
+      //when & then
+      Assertions.assertThatThrownBy(() -> {
+        verifyLottoResult(null, new ArrayList<>());
+      }).isInstanceOf(Exception.class)
+      .hasMessageContaining("당첨번호가 입력되지 않았습니다.");
+  }
+  @Test
+  void 당첨번호_갯수가_6개가아니면_throw_Exception() {
+      //when & then
+      Assertions.assertThatThrownBy(() -> {
+        verifyLottoResult(Arrays.asList(1,2,3), new ArrayList<>());
+      }).isInstanceOf(Exception.class)
+      .hasMessageContaining("당첨번호 갯수는 6개여야 합니다.");
+  }
+
+  @Test
+  void 긁은복권모음이_null이면_throw_Exception() {
+      //when & then
+      Assertions.assertThatThrownBy(() -> {
+        verifyLottoResult(Arrays.asList(1,2,3,4,5,6), null);
+      }).isInstanceOf(Exception.class)
+      .hasMessageContaining("긁은복권모음이 입력되지 않았습니다");
+  }
+  @Test
+  void 긁은복권모음이_없으면_throw_Exception() {
+      //when & then
+      Assertions.assertThatThrownBy(() -> {
+        verifyLottoResult(Arrays.asList(1,2,3,4,5,6), new ArrayList<>());
+      }).isInstanceOf(Exception.class)
+      .hasMessageContaining("긁은 복권이 없습니다.");
   }
 }
